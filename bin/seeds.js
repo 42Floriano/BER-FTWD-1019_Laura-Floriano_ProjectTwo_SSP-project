204bacca598b47561bd8bef0d3d0af03a2a2d782
Laura Ningel <nofear6@Lauras-MacBook-Pro-6.local>
2019-11-18 15:29:29 +0100

    User model created

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,4 +1,4 @@
-// Seeds file that remove all users and create 2 new users
+// Seeds file that removes all users and creates 2 new users
 
 // To execute this seed, run from the root of the project
 // $ node bin/seeds.js
@@ -10,7 +10,9 @@ const User = require("../models/User");
 const bcryptSalt = 10;
 
 mongoose
-  .connect('mongodb://localhost/ssp-project', {useNewUrlParser: true})
+  .connect('mongodb://localhost/ssp-project', {
+    useNewUrlParser: true
+  })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -18,30 +20,41 @@ mongoose
     console.error('Error connecting to mongo', err)
   });
 
-let users = [
-  {
+let users = [{
     username: "alice",
     password: bcrypt.hashSync("alice", bcrypt.genSaltSync(bcryptSalt)),
+    email: "testmailOne",
+    interests: ["Sailing", "Searching Mushrooms", "Baking Banana Bread"],
+    languages: ["French", "German"],
+    picture: "test",
+    id: "1234",
+    // skills_Ids: Array
   },
   {
     username: "bob",
     password: bcrypt.hashSync("bob", bcrypt.genSaltSync(bcryptSalt)),
+    email: "testmailTwo",
+    interests: ["Ice skating", "Jazz Dance"],
+    languages: ["German", "English"],
+    picture: "test",
+    id: "5678",
+    // skills_Ids: Array
   }
 ]
 
 User.deleteMany()
-.then(() => {
-  return User.create(users)
-})
-.then(usersCreated => {
-  console.log(`${usersCreated.length} users created with the following id:`);
-  console.log(usersCreated.map(u => u._id));
-})
-.then(() => {
-  // Close properly the connection to Mongoose
-  mongoose.disconnect()
-})
-.catch(err => {
-  mongoose.disconnect()
-  throw err
-})
+  .then(() => {
+    return User.create(users)
+  })
+  .then(usersCreated => {
+    console.log(`${usersCreated.length} users created with the following id:`);
+    console.log(usersCreated.map(u => u._id));
+  })
+  .then(() => {
+    // Close properly the connection to Mongoose
+    mongoose.disconnect()
+  })
+  .catch(err => {
+    mongoose.disconnect()
+    throw err
+  })
--- models/User.js
+++ models/User.js
@@ -1,14 +1,31 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: String,
-  password: String
-}, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
-  }
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3,
+    maxlength: 13
+  },
+  password: {
+    type: String,
+    validate: {
+      validator: userPassword => {
+        if (userPassword.length < 8) {
+          return false;
+        }
+        return true;
+      },
+      message: "Please enter password with 8+ characters!"
+    }
+  },
+  email: String,
+  interests: Array,
+  languages: Array,
+  picture: String,
+  skills_Ids: Array
 });
 
 const User = mongoose.model('User', userSchema);
