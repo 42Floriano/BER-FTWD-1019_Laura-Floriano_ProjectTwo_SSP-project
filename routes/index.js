e8c5cb9d47840cf2bbe7a64f9be5a7b7e97176a0
Laura Ningel <nofear6@Lauras-MacBook-Pro-6.local>
2019-11-19 15:15:39 +0100

    day-2

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,48 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get("/skills", (req, res, next) => {
+  Skill.find({})
+    .then(documents => {
+      res.render("skills/skills", {
+        skills: documents
+      })
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
+// router.get("/search", (req, res, next) => {
+//   const searchStr = req.query.q;
+
+//   Skill.find({})
+//     .then
+// });
+
+
+// const filtered = players.filter(player => {
+//   if (player.name.toLowerCase().includes(searchString.toLowerCase())) {
+//       return true;
+//   }
+// });
+
+// res.render("players.hbs", {
+//   playersList: filtered
+// });
+
+
+router.get("/details:id", (req, res, next) => {
+  Skill.findById(req.params.id)
+    .then(theSkill => {
+      res.render("skills/details", {
+        skill: theSkill
+      })
+    })
+    .catch(err => {
+      console.log(err)
+    });
+})
+
+
 module.exports = router;
