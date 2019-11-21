2847afdb788e6fe3d1669e314c9737555d55bee6
Floriano Albertini <albertini@Florianos-MacBook-Pro.local>
2019-11-21 17:39:47 +0100

    bitchplease

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -14,3 +14,21 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+
+let slideIndex = 0;
+carousel();
+
+function carousel() {
+  let i;
+  let x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  if (x[slideIndex-1]) {
+    x[slideIndex-1].style.display = "block";
+  }
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
